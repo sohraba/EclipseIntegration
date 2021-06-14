c5ef5caa6c13459e65850f0cdb57f354d4bc44e9
Sohrab <sohrab.ahmad@ihsmarkit.com>
2021-06-14 22:20:26 +0530

    First Commit

---
+++ src/main/java/org/seleniumhq/selenium/QADemo/App.java
@@ -0,0 +1,13 @@
+package org.seleniumhq.selenium.QADemo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
