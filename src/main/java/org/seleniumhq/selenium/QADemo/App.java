f21487131c225671f66753638c6fc059355a769e
Sohrab <sohrab.ahmad@ihsmarkit.com>
2021-06-15 20:23:15 +0530

    update

--- src/main/java/org/seleniumhq/selenium/QADemo/App.java
+++ src/main/java/org/seleniumhq/selenium/QADemo/App.java
@@ -8,7 +8,8 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hi212 World!" );
+        int x=10;
         System.out.println("Testing");
+        System.out.println( "Hi212 World!" );
     }
 }
